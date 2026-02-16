c6783f2beaf01339a7331623f1378d98edcf9634
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2026-02-16 15:43:28 +0600

    Update c.sh

--- c.sh
+++ c.sh
@@ -16,7 +16,7 @@ echo "Installing required packages..."
 sudo apt update
 sudo apt install -y git-core gnupg flex bison gperf build-essential \
     zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 \
-    lib32ncurses6-dev x11proto-core-dev libx11-dev lib32z1-dev ccache \
+    x11proto-core-dev libx11-dev lib32z1-dev ccache \
     libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig python3 python3-pip
 
 # ----------------------------
